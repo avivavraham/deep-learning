8093c9c96599d312f1637457da0b497b07589213
avivavraham <avivavraham@mail.tau.ac.il>
2023-05-18 21:17:45 +0300

    did Forward Pass in Back Propagation function in backprop_network.py and fixes some errors in all files

--- backprop_main.py
+++ backprop_main.py
@@ -1,10 +1,7 @@
 import backprop_data
-
 import backprop_network
 
-
-
-training_data, test_data = backprop_data.load(train_size=10000,test_size=5000)
+training_data, test_data = backprop_data.load(train_size=10000, test_size=5000)
 
 net = backprop_network.Network([784, 40, 10])
 
